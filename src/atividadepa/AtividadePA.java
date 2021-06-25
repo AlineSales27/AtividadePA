1eb36ad7c7579935cf973791f53dda103b2f79cc
AlineSales27 <77058468+AlineSales27@users.noreply.github.com>
2021-06-25 10:30:42 -0300

    initial commit

---
+++ src/atividadepa/AtividadePA.java
@@ -0,0 +1,23 @@
+
+package atividadepa;
+
+public class AtividadePA {
+    String Sobrenome;
+    String CPF;
+    String Nome;
+    String Endereco;
+    int idade;
+      
+
+public void ExibirDados(String Nome, String Sobrenome, String CPF, String Endereco, int idade){
+    System.out.println("O nome é: " + Nome);
+    System.out.println("O sobrenome é: " + Sobrenome);
+    System.out.println("O cpf é: " + CPF);
+    System.out.println("O endereco é: " + Endereco);
+    System.out.println("A idade é: " + idade);
+}
+      
+        
+  
+    
+}
---
+++ src/atividadepa/PrincipalPessoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadepa;
+
+/**
+ *
+ * @author User
+ */
+public class PrincipalPessoa {
+    
+    public static void main(String[] args) {
+    AtividadePA Pessoas = new AtividadePA();
+    Pessoas.ExibirDados("Aline","Sales", "000.000.000-00", "Rua João Silva", 20);
+    }
+}
+
